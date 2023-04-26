2ec263253fa45e67ad2e527e3eb870d426b5bf8c
Tyler <td20147@gmail.com>
2023-04-26 17:13:26 -0400

    finally got database working, accidently initialized one with incorrect formatting so had to delete and re initialize with correct names

--- website/models.py
+++ website/models.py
@@ -6,7 +6,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True) # primary key
     email = db.Column(db.String(150), unique=True) # unique email
     password = db.Column(db.String(150))
-    firstName = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
     notes = db.relationship('Note') # relationship with Note table
 
 class Note(db.Model):
