cf137ceb4fd71b489905ec7d3777bb567d3f88a5
Tyler <td20147@gmail.com>
2023-04-25 23:25:45 -0400

    created forms for login and sign up page, now take post requests

--- website/auth.py
+++ website/auth.py
@@ -1,13 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@views.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+
+    # data = request.form
+    # print(data)
     return render_template("login.html")
 
 
-@views.route("/sign-up")
+@auth.route("/sign-up", methods=['GET', 'POST'])
 def sign_up():
     return render_template("sign_up.html")
     
